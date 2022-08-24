4e8fe63b8e77c9b81a5dc8fed115afc3a5074a59
Krishan Patel <kp3099@gmail.com>
2022-08-24 15:06:15 -0400

    created empty fragments

--- app/src/main/java/com/play/freso/news_android/ui/NewsActivity.kt
+++ app/src/main/java/com/play/freso/news_android/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.play.freso.news_android
+package com.play.freso.news_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.play.freso.news_android.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
---
+++ app/src/main/java/com/play/freso/news_android/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.play.freso.news_android.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.play.freso.news_android.R
+
+class SavedNewsFragment: Fragment(R.layout.fragment_saved_news) {
+}
